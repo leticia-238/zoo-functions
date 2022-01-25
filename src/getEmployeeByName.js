3411321ce6c7b089b16e0dbe3f1e9960f1d24a64
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-01-25 17:16:14 -0300

    Implementa a função getEmployeebyName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+const getEmployeeByName = (employeeName) => {
+  if (employeeName) {
+    return employees.find(({ firstName, lastName }) =>
+      employeeName === firstName || employeeName === lastName);
+  }
+  return {};
+};
 
 module.exports = getEmployeeByName;
