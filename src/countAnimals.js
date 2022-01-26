1d56c24ce655422c2f7bde63edacc9d8708d7c41
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-01-26 16:00:13 -0300

    Implementa a função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const animalsBySex = (residents, sex) => residents.filter((resident) => resident.sex === sex);
+
+const allAnimals = (acc, { name, residents }) => {
+  acc[name] = residents.length;
+  return acc;
+};
+
+const countAnimals = (objeto) => {
+  if (objeto) {
+    const { specie, sex } = objeto;
+    const { residents } = species.find(({ name }) => name === specie);
+    return sex ? animalsBySex(residents, sex).length : residents.length;
+  }
+  return species.reduce(allAnimals, {});
+};
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
