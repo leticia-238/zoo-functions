a4a6ceabd11b197ed479518bb4e7dac652c63d7f
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-01-27 17:16:33 -0300

    Implementa a função getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,21 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const oldestAnimal = (actualAnimal, nextAnimal) => {
+  if (actualAnimal.age < nextAnimal.age) {
+    return 1;
+  }
+  if (actualAnimal.age > nextAnimal.age) {
+    return -1;
+  }
+  return 0;
+};
+
+const getOldestFromFirstSpecies = (employeeId) => {
+  const specieId = employees.find(({ id }) => employeeId === id)
+    .responsibleFor[0];
+  const animals = species.find(({ id }) => specieId === id).residents;
+  const animalObject = animals.sort(oldestAnimal)[0];
+  return Object.values(animalObject);
+};
 
 module.exports = getOldestFromFirstSpecies;
