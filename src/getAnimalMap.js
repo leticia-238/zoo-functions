b4312a6c8cb012c71bef425abe6520ff833a19a5
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-01-27 11:09:50 -0300

    Implementa a função getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,33 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+const sortBySex = (options, residents) => residents
+  .filter((resident) => resident.sex === options.sex)
+  .map((resident) => resident.name);
+
+const getAnimalNames = (options, residents) => {
+  let animalNames = residents.map((resident) => resident.name);
+  if (options.sex) {
+    animalNames = sortBySex(options, residents);
+  }
+  if (options.sorted) {
+    animalNames = animalNames.sort();
+  }
+  return animalNames;
+};
+
+const getAnimalMap = (options) => {
+  const animalLocations = {};
+  species.forEach(({ location }) => { animalLocations[location] = []; });
+  if (options && options.includeNames) {
+    species.forEach(({ name, location, residents }) => {
+      const specieName = {};
+      specieName[name] = getAnimalNames(options, residents);
+      animalLocations[location].push(specieName);
+    });
+    return animalLocations;
+  }
+  species.forEach(({ name, location }) => { animalLocations[location].push(name); });
+  return animalLocations;
+};
 
 module.exports = getAnimalMap;
