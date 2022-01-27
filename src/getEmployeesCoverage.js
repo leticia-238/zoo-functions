34ae03340ef0ce7202e8fb5e9a67ba14d74115f5
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-01-27 18:57:57 -0300

    Implementa a função getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,41 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const options = {
+  name: (name) => employees.find(({ firstName, lastName }) =>
+    firstName === name || lastName === name || `${firstName} ${lastName}` === name),
+  id: (id) => employees.find((employee) => employee.id === id),
+};
+
+const searchSpeciesById = (idList) => {
+  const specieNames = [];
+  const specieLocations = [];
+  species.forEach(({ id, name, location }) => {
+    if (idList.includes(id)) {
+      specieNames.push(name);
+      specieLocations.push(location);
+    }
+  });
+  return { specieNames, specieLocations };
+};
+
+const getEmployeeInfos = ({ id, firstName, lastName, responsibleFor }) => ({
+  id,
+  fullName: `${firstName} ${lastName}`,
+  species: searchSpeciesById(responsibleFor).specieNames,
+  locations: searchSpeciesById(responsibleFor).specieLocations,
+});
+
+const getEmployeesCoverage = (object) => {
+  if (object) {
+    const option = Object.keys(object)[0];
+    const optionValue = Object.values(object)[0];
+    const employee = options[option](optionValue);
+    if (employee) {
+      return getEmployeeInfos(employee);
+    }
+    throw new Error('Informações inválidas');
+  }
+  return employees.map((employee) => getEmployeeInfos(employee));
+};
 
 module.exports = getEmployeesCoverage;
